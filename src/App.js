21dbc014202a93dd617ee3477971253eae9fa70c
dhruza88 <davidhruza@gmail.com>
2022-08-31 10:33:43 -0700

    updated file structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
-import Search from "./components/search/search";
-
-import CurrentWeather from "./components/current-weather/current-weather";
+import Search from "./weather-components/search/search";
+import CurrentWeather from "./weather-components/current-weather/current-weather";
 // import Forecast from "./components/forecast/forecast";
 import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 import "./App.css";
